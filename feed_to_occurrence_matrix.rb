a62f8abf333c469b4db351cbd4ea2e02b21d27e4
mat <matthew.kelcey@gmail.com>
2009-07-27 23:08:18 +1000

    remove terms for word occurence matrix if they appear in only one document in corpus

--- feed_to_occurrence_matrix.rb
+++ feed_to_occurrence_matrix.rb
@@ -26,15 +26,15 @@ end
 
 require 'set'
 
-term_to_idx = TermToIdx.new
 doc_term_freq = []
+num_non_zero_entries = 0
+term_to_docs = {}
 all_terms = Set.new
-num_entries = 0
 
-STDIN.each do |line|
+# get terms from stdin
+STDIN.each_with_index do |line, doc|
 	url, date, text = line.split '|'
 
-	# get terms from line
 	terms = text.gsub(/[^a-zA-Z0-9 ]/,' ').
 			split(/\s+/).
 			select { |w| w.length >1 }.
@@ -44,16 +44,42 @@ STDIN.each do |line|
 
 	term_freq = {}
 	term_freq.default = 0
-	terms.each { |term| term_freq[term_to_idx.idx_of(term)] += 1 }
+	terms.each do |term| 
+		term_freq[term] += 1 
+		term_to_docs[term] ||= Set.new
+		term_to_docs[term] << doc
+	end
+
 	doc_term_freq << term_freq
-	num_entries += term_freq.size
+	num_non_zero_entries += term_freq.size
 
 end
 
+# remove entries that correspond to a term that only appears in one document
+single_entry_terms = []
+term_to_docs.each do |term, docs|
+	single_entry_terms << term if docs.size==1
+end
+single_entry_terms.each do |term|
+	all_terms.delete term
+	doc_term_freq.each { |tf| tf.delete term } # (though will only be one to has it removed)
+	num_non_zero_entries -= 1
+end
+
+# convert from terms to idxs
+term_to_idx = TermToIdx.new
+doc_term_freq = doc_term_freq.collect do |term_freq|
+	term_freq2 = {}
+	term_freq.each do |term,freq|
+		term_freq2[term_to_idx.idx_of(term)] = freq
+	end
+	term_freq2
+end
+
+# output sparse format
 num_terms = all_terms.size
 num_docs = doc_term_freq.size
-puts "#{num_terms} #{num_docs} #{num_entries}"
-
+puts "#{num_terms} #{num_docs} #{num_non_zero_entries}"
 doc_term_freq.each do |term_freq|
 	puts "#{term_freq.size}"
 	term_freq.each do |term,freq|
